d0e6db50c6c9734d1fe7dab9fb0415abb6b12045
LaubetBeltran <laubetpop@gmail.com>
2021-02-26 13:37:49 -0600

    Test airbnb// Errores sólo de firebase

--- src/modal.js
+++ src/modal.js
@@ -1,46 +1,45 @@
-import { onNavigate } from "./routing.js";
-import { ErrorAccount, ErrorLoginMail } from "./modalError.js"
+import { ErrorAccount, ErrorLoginMail } from './modalError.js';
 // Tache para cerrar el modal
-let modal = document.querySelectorAll(".modal")[0];
-let modalContainer = document.getElementById("modal-container");
-let modalAlert= document.getElementById('modal-alert');
+const modal = document.querySelectorAll('.modal')[0];
+const modalContainer = document.getElementById('modal-container');
+const modalAlert = document.getElementById('modal-alert');
 
-
-export const openModal = (mensajeError) => {   
-    // visibilidad de la transparencia del modal
-    modalContainer.style.opacity = "1";
-    modalContainer.style.visibility = "visible";
-    // toggle, quita o pone la clase "modal-close"
-    modal.classList.toggle ("modal-close");
-    modalAlert.innerHTML= mensajeError;
-    closeModal();
-    
-    if(mensajeError == ErrorLoginMail){
-        let otherUserLogin = document.getElementById('otherUser');
-        otherUserLogin.addEventListener('click', () => {
-            onNavigate('/account'); 
-            closeModalLink();
-        }); 
-    }else if(mensajeError == ErrorAccount){
-        document.getElementById('modalErrorLogin-link').addEventListener('click', ()=>{
-            onNavigate('/');
-            closeModalLink();
-        });
-        document.getElementById('modalErrorAccount-link').addEventListener('click', ()=>{
-            onNavigate('/account');
-            closeModalLink();
-        });  
-    }    
+function closeModalLink() {
+  modal.classList.toggle('modal-close');
+  modalContainer.style.opacity = '0';
+  modalContainer.style.visibility = 'hidden';
 }
 
-export function closeModal (){
-    let close = document.getElementById("close");
-    close.addEventListener("click", closeModalLink);
-};
+export function closeModal() {
+  const close = document.getElementById('close');
+  close.addEventListener('click', closeModalLink);
+}
 
-
-function closeModalLink () {
-    modal.classList.toggle ("modal-close");
-    modalContainer.style.opacity = "0";
-    modalContainer.style.visibility = "hidden";
+export const openModal = (mensajeError) => {
+  // visibilidad de la transparencia del modal
+  modalContainer.style.opacity = '1';
+  modalContainer.style.visibility = 'visible';
+  // toggle, quita o pone la clase "modal-close"
+  modal.classList.toggle('modal-close');
+  modalAlert.innerHTML = mensajeError;
+  closeModal();
+  if (mensajeError === ErrorLoginMail) {
+    const otherUserLogin = document.getElementById('otherUser');
+    otherUserLogin.addEventListener('click', () => {
+      const accountlink = document.getElementById('acc');
+      accountlink.click();
+      closeModalLink();
+    });
+  } else if (mensajeError === ErrorAccount) {
+    document.getElementById('modalErrorLogin-link').addEventListener('click', () => {
+      const loginlink = document.getElementById('log');
+      loginlink.click();
+      closeModalLink();
+    });
+    document.getElementById('modalErrorAccount-link').addEventListener('click', () => {
+      const accountlink = document.getElementById('acc');
+      accountlink.click();
+      closeModalLink();
+    });
+  }
 };
